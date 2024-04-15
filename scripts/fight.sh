441fb03213bc8ea71c27d34941d4237a28ccc3c9
Asten <>
2024-04-15 15:11:40 +0800

    add new scripts

---
+++ scripts/fight.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+pushd $(dirname "$0")/..
+
+export RPC_URL="https://api.cartridge.gg/x/warpack-master/katana";
+
+export WORLD_ADDRESS=$(cat ./manifests/deployments/KATANA.json | jq -r '.world.address')
+
+export ACTIONS_ADDRESS=$(cat ./manifests/deployments/KATANA.json | jq -r '.contracts[] | select(.name == "warpack_masters::systems::actions::actions" ).address')
+
+
+echo "---------------------------------------------------------------------------"
+echo world : $WORLD_ADDRESS
+echo " "
+echo actions : $ACTIONS_ADDRESS
+echo "---------------------------------------------------------------------------"
+
+sozo execute --world $WORLD_ADDRESS $ACTIONS_ADDRESS fight --wait --rpc-url $RPC_URL \
+	--account-address 0x32f35835b7cca077a83fd5e53c6036d7bb36d8d9542473198525399f568b7f6 \
+	--private-key 0x3454b66f83b00e67610adad5631f437a71834ad9dc0481ce36f1062945b955e
