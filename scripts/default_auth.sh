51a1278e283af0126fb12605b5b3de2888f2120d
Asten <0xasten@gmail.com>
2024-08-12 17:46:08 +0800

    update auth script

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -29,93 +29,93 @@ echo "--------------------------------------------------------------------------
 
 # enable system -> models authorizations
 sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
-  BackpackGrids,$ACTIONS_ADDRESS \
-  Character,$ACTIONS_ADDRESS \
-  CharacterItemStorage,$ACTIONS_ADDRESS \
-  CharacterItemsStorageCounter,$ACTIONS_ADDRESS \
-  CharacterItemInventory,$ACTIONS_ADDRESS \
-  CharacterItemsInventoryCounter,$ACTIONS_ADDRESS \
-  DummyCharacter,$ACTIONS_ADDRESS \
-  DummyCharacterCounter,$ACTIONS_ADDRESS \
-  DummyCharacterItem,$ACTIONS_ADDRESS \
-  DummyCharacterItemsCounter,$ACTIONS_ADDRESS \
-  Item,$ACTIONS_ADDRESS \
-  ItemsCounter,$ACTIONS_ADDRESS \
-  Shop,$ACTIONS_ADDRESS \
-  BattleLog,$ACTIONS_ADDRESS \
-  BattleLogCounter,$ACTIONS_ADDRESS\
-  NameRecord,$ACTIONS_ADDRESS
+  model:Warpacks-BackpackGrids,$ACTIONS_ADDRESS \
+  model:Warpacks-Character,$ACTIONS_ADDRESS \
+  model:Warpacks-CharacterItemStorage,$ACTIONS_ADDRESS \
+  model:Warpacks-CharacterItemsStorageCounter,$ACTIONS_ADDRESS \
+  model:Warpacks-CharacterItemInventory,$ACTIONS_ADDRESS \
+  model:Warpacks-CharacterItemsInventoryCounter,$ACTIONS_ADDRESS \
+  model:Warpacks-DummyCharacter,$ACTIONS_ADDRESS \
+  model:Warpacks-DummyCharacterCounter,$ACTIONS_ADDRESS \
+  model:Warpacks-DummyCharacterItem,$ACTIONS_ADDRESS \
+  model:Warpacks-DummyCharacterItemsCounter,$ACTIONS_ADDRESS \
+  model:Warpacks-Item,$ACTIONS_ADDRESS \
+  model:Warpacks-ItemsCounter,$ACTIONS_ADDRESS \
+  model:Warpacks-Shop,$ACTIONS_ADDRESS \
+  model:Warpacks-BattleLog,$ACTIONS_ADDRESS \
+  model:Warpacks-BattleLogCounter,$ACTIONS_ADDRESS\
+  model:Warpacks-NameRecord,$ACTIONS_ADDRESS
 
 sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
-  BackpackGrids,$DUMMY_STSTEM_ADDRESS \
-  Character,$DUMMY_STSTEM_ADDRESS \
-  CharacterItemStorage,$DUMMY_STSTEM_ADDRESS \
-  CharacterItemsStorageCounter,$DUMMY_STSTEM_ADDRESS \
-  CharacterItemInventory,$DUMMY_STSTEM_ADDRESS \
-  CharacterItemsInventoryCounter,$DUMMY_STSTEM_ADDRESS \
-  DummyCharacter,$DUMMY_STSTEM_ADDRESS \
-  DummyCharacterCounter,$DUMMY_STSTEM_ADDRESS \
-  DummyCharacterItem,$DUMMY_STSTEM_ADDRESS \
-  DummyCharacterItemsCounter,$DUMMY_STSTEM_ADDRESS \
-  Item,$DUMMY_STSTEM_ADDRESS \
-  ItemsCounter,$DUMMY_STSTEM_ADDRESS \
-  Shop,$DUMMY_STSTEM_ADDRESS \
-  BattleLog,$DUMMY_STSTEM_ADDRESS \
-  BattleLogCounter,$DUMMY_STSTEM_ADDRESS\
-  NameRecord,$DUMMY_STSTEM_ADDRESS
+  model:Warpacks-BackpackGrids,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-Character,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemStorage,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsStorageCounter,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemInventory,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsInventoryCounter,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacter,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterCounter,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItem,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItemsCounter,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-Item,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-ItemsCounter,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-Shop,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-BattleLog,$DUMMY_STSTEM_ADDRESS \
+  model:Warpacks-BattleLogCounter,$DUMMY_STSTEM_ADDRESS\
+  model:Warpacks-NameRecord,$DUMMY_STSTEM_ADDRESS
 
 sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
-  BackpackGrids,$FIGHT_STSTEM_ADDRESS \
-  Character,$FIGHT_STSTEM_ADDRESS \
-  CharacterItemStorage,$FIGHT_STSTEM_ADDRESS \
-  CharacterItemsStorageCounter,$FIGHT_STSTEM_ADDRESS \
-  CharacterItemInventory,$FIGHT_STSTEM_ADDRESS \
-  CharacterItemsInventoryCounter,$FIGHT_STSTEM_ADDRESS \
-  DummyCharacter,$FIGHT_STSTEM_ADDRESS \
-  DummyCharacterCounter,$FIGHT_STSTEM_ADDRESS \
-  DummyCharacterItem,$FIGHT_STSTEM_ADDRESS \
-  DummyCharacterItemsCounter,$FIGHT_STSTEM_ADDRESS \
-  Item,$FIGHT_STSTEM_ADDRESS \
-  ItemsCounter,$FIGHT_STSTEM_ADDRESS \
-  Shop,$FIGHT_STSTEM_ADDRESS \
-  BattleLog,$FIGHT_STSTEM_ADDRESS \
-  BattleLogCounter,$FIGHT_STSTEM_ADDRESS\
-  NameRecord,$FIGHT_STSTEM_ADDRESS
+  model:Warpacks-BackpackGrids,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-Character,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemStorage,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsStorageCounter,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemInventory,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsInventoryCounter,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacter,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterCounter,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItem,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItemsCounter,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-Item,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-ItemsCounter,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-Shop,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-BattleLog,$FIGHT_STSTEM_ADDRESS \
+  model:Warpacks-BattleLogCounter,$FIGHT_STSTEM_ADDRESS\
+  model:Warpacks-NameRecord,$FIGHT_STSTEM_ADDRESS
 
 sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
-  BackpackGrids,$ITEM_STSTEM_ADDRESS \
-  Character,$ITEM_STSTEM_ADDRESS \
-  CharacterItemStorage,$ITEM_STSTEM_ADDRESS \
-  CharacterItemsStorageCounter,$ITEM_STSTEM_ADDRESS \
-  CharacterItemInventory,$ITEM_STSTEM_ADDRESS \
-  CharacterItemsInventoryCounter,$ITEM_STSTEM_ADDRESS \
-  DummyCharacter,$ITEM_STSTEM_ADDRESS \
-  DummyCharacterCounter,$ITEM_STSTEM_ADDRESS \
-  DummyCharacterItem,$ITEM_STSTEM_ADDRESS \
-  DummyCharacterItemsCounter,$ITEM_STSTEM_ADDRESS \
-  Item,$ITEM_STSTEM_ADDRESS \
-  ItemsCounter,$ITEM_STSTEM_ADDRESS \
-  Shop,$ITEM_STSTEM_ADDRESS \
-  BattleLog,$ITEM_STSTEM_ADDRESS \
-  BattleLogCounter,$ITEM_STSTEM_ADDRESS\
-  NameRecord,$ITEM_STSTEM_ADDRESS
+  model:Warpacks-BackpackGrids,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-Character,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemStorage,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsStorageCounter,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemInventory,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsInventoryCounter,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacter,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterCounter,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItem,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItemsCounter,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-Item,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-ItemsCounter,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-Shop,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-BattleLog,$ITEM_STSTEM_ADDRESS \
+  model:Warpacks-BattleLogCounter,$ITEM_STSTEM_ADDRESS\
+  model:Warpacks-NameRecord,$ITEM_STSTEM_ADDRESS
 
 sozo auth grant --world $WORLD_ADDRESS --rpc-url $STARKNET_RPC_URL --wait writer \
-  BackpackGrids,$SHOP_STSTEM_ADDRESS \
-  Character,$SHOP_STSTEM_ADDRESS \
-  CharacterItemStorage,$SHOP_STSTEM_ADDRESS \
-  CharacterItemsStorageCounter,$SHOP_STSTEM_ADDRESS \
-  CharacterItemInventory,$SHOP_STSTEM_ADDRESS \
-  CharacterItemsInventoryCounter,$SHOP_STSTEM_ADDRESS \
-  DummyCharacter,$SHOP_STSTEM_ADDRESS \
-  DummyCharacterCounter,$SHOP_STSTEM_ADDRESS \
-  DummyCharacterItem,$SHOP_STSTEM_ADDRESS \
-  DummyCharacterItemsCounter,$SHOP_STSTEM_ADDRESS \
-  Item,$SHOP_STSTEM_ADDRESS \
-  ItemsCounter,$SHOP_STSTEM_ADDRESS \
-  Shop,$SHOP_STSTEM_ADDRESS \
-  BattleLog,$SHOP_STSTEM_ADDRESS \
-  BattleLogCounter,$SHOP_STSTEM_ADDRESS\
-  NameRecord,$SHOP_STSTEM_ADDRESS
+  model:Warpacks-BackpackGrids,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-Character,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemStorage,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsStorageCounter,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemInventory,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-CharacterItemsInventoryCounter,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacter,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterCounter,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItem,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-DummyCharacterItemsCounter,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-Item,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-ItemsCounter,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-Shop,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-BattleLog,$SHOP_STSTEM_ADDRESS \
+  model:Warpacks-BattleLogCounter,$SHOP_STSTEM_ADDRESS\
+  model:Warpacks-NameRecord,$SHOP_STSTEM_ADDRESS
 
 echo "Default authorizations have been successfully set."
