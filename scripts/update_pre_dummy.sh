7afb72fb866e069070c5631392fee7f215d04c03
Asten <0xasten@gmail.com>
2024-08-12 09:32:42 +0800

    new update script

--- scripts/update_pre_dummy.sh
+++ scripts/update_pre_dummy.sh
@@ -15,6 +15,4 @@ echo actions : $ACTIONS_ADDRESS
 echo "---------------------------------------------------------------------------"
 
 
-# Generated item commands
-# sozo execute --world $WORLD_ADDRESS $ACTIONS_ADDRESS update_prefine_dummy -c 7,1 --wait --rpc-url $STARKNET_RPC_URL
-sozo execute --world $WORLD_ADDRESS $ACTIONS_ADDRESS update_prefine_dummy -c 4,1 --wait --rpc-url $STARKNET_RPC_URL
+sozo execute --world $WORLD_ADDRESS $ACTIONS_ADDRESS update_prefine_dummy -c 1,19,0x446f74746965,0,21,1,4,2,0,2,2,2,0,2,2,4,0,2,4,5,0,2,6,1,0,2,6,3,0,2,4,0,0,2,2,0,0,2,6,5,0,22,2,0,0,29,4,0,0,14,6,1,0,25,7,1,0,28,2,4,0,27,3,4,0,26,6,4,0,19,4,3,0,11,4,5,0,11,4,6,0,10,5,5,0,20,5,6,0 --wait --rpc-url $STARKNET_RPC_URL
