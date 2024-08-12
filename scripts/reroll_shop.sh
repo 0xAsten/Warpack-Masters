cad5a7df43e430a3c7fcf1e96b7dca285d935c95
Asten <0xasten@gmail.com>
2024-08-12 17:56:41 +0800

    update other scripts

--- scripts/reroll_shop.sh
+++ scripts/reroll_shop.sh
@@ -4,17 +4,17 @@ pushd $(dirname "$0")/..
 
 : "${STARKNET_RPC_URL:?Environment variable STARKNET_RPC_URL must be set}"
 
-export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./manifests/dev/deployment/manifest.json | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.contracts[] | select(.name == "warpack_masters::systems::actions::actions" ).address')
+export SHOP_STSTEM_ADDRESS=$(cat ./manifests/dev/deployment/manifest.json | jq -r '.contracts[] | select(.tag == "Warpacks-shop_system" and .kind == "DojoContract").address')
 
 
 echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS
 echo " "
-echo actions : $ACTIONS_ADDRESS
+echo shop system : $SHOP_STSTEM_ADDRESS
 echo "---------------------------------------------------------------------------"
 
-sozo execute --world $WORLD_ADDRESS $ACTIONS_ADDRESS reroll_shop --wait --rpc-url $STARKNET_RPC_URL \
-	--account-address 0x24564a69b21a2683b82b0211644577213644b1d832b50b444df2c40e0f5253b \
-	--private-key 0x7957b76cecb7995b071c16120243268a64e6fa8cf5310d30400b56d7de97adc
+sozo execute --world $WORLD_ADDRESS $SHOP_STSTEM_ADDRESS reroll_shop --wait --rpc-url $STARKNET_RPC_URL \
+	--account-address 0x1e59eb74ce98fced4e9b10cb8d9db58f856194da24984fd64193e0d787ce519 \
+	--private-key 0x76468ff8e97cf4ad25412f6134f3b3ce835ee9732da92b3b6b1974de76b8975
